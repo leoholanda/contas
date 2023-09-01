2831593f2f959890e333870da26f0796c02c09b2
Leonardo Holanda Arruda <leoholanda23@gmail.com>
2023-09-01 15:40:03 -0400

    Add api-contas

---
+++ api-contas/src/test/java/com/deliverit/apicontas/ApiContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverit.apicontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
